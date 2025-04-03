a66cb12d674a75562f9c191563374fcebd4785a7
Irshad138 <125353040+Irshad138@users.noreply.github.com>
2025-04-03 23:07:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Dynamic Website</title>
+    <title>PHP Dynamic Website nahi hai </title>
     <link rel="stylesheet" href="css/style.css"/>
 
 </head>
