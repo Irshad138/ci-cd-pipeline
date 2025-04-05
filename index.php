9f8e01be9c544a96703eaaf7af5a15c8b00847e4
Irshad138 <125353040+Irshad138@users.noreply.github.com>
2025-04-05 15:11:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             </ul>
         </nav>
         <div class="main-heading">
-            <h1>Create Prespectives With Virtual Reality</h1>
+            <h1>Create Prespectives With Virtual Realityyyyyyyyy</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint provident consectetur ducimus. Blanditiis, culpa!</p>
             <a class="main-btn" href="#">Contact</a>
         </div>
